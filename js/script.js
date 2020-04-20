0449e7546d7e94cffa8558b5c13eb2890a203d3f
Jonny Hunter <hunter.jonny@yahoo.co.uk>
2020-04-20 22:00:05 +0100

    more dark mode style added

--- js/script.js
+++ js/script.js
@@ -5,4 +5,8 @@ var element = document.querySelectorAll(".card");
   for (i = 0; i < element.length; ++i) {
     element[i].classList.toggle("card__dark");
   }
+  var element = document.querySelectorAll(".card-container");
+  for (i = 0; i < element.length; ++i) {
+    element[i].classList.toggle("card-container__dark");
+  }
 }
