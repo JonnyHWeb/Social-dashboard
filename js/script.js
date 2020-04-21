bc31e757f6b2a882299d6bfeb43426cb19ce722b
Jonny Hunter <hunter.jonny@yahoo.co.uk>
2020-04-21 20:42:38 +0100

    add script for pos/neg numbers

--- js/script.js
+++ js/script.js
@@ -1,12 +1,36 @@
+socialUpdater();
+
+
 //Toggle the theme between dark/light
 function themefunction() {
 
-var element = document.querySelectorAll(".card");
+  var element = document.querySelectorAll(".container");
   for (i = 0; i < element.length; ++i) {
-    element[i].classList.toggle("card__dark");
+    element[i].classList.toggle("container__dark");
   }
-  var element = document.querySelectorAll(".card-container");
-  for (i = 0; i < element.length; ++i) {
-    element[i].classList.toggle("card-container__dark");
+}
+
+
+
+//Update social scores
+function socialUpdater() {
+var numbers= document.querySelectorAll('.social-update');
+
+for (i = 0; i < numbers.length; ++i) {
+  
+  var a = numbers[i].textContent;
+  if(a.charAt(0)=='-'){
+    numbers[i].style.color = "red";
+    a = a.substring(1);
+    numbers[i].firstChild.data = a;
+    numbers[i].classList.add("social-update__negative");
+  } 
+  else if (a.charAt(0)=='0') {
+     numbers[i].style.color = "black";
+   }
+    else {
+      numbers[i].style.color = "green";
+      numbers[i].classList.add("social-update__positive");
+    }
   }
 }
